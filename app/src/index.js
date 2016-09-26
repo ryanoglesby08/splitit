fc8fb3be2215438cbfeaf9087572ff23c19f34bc
Ryan Oglesby <ryan.oglesby08@gmail.com>
2016-09-26 17:25:13 -0500

    Initial commit - bootstrapping with es2016 + Babel + webpack

---
+++ app/src/index.js
@@ -0,0 +1,14 @@
+/* global document */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+require('../stylesheet/application.scss');
+
+ReactDOM.render(
+  <div>
+    Hello world!
+  </div>,
+  document.getElementById('root')
+);
+
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './app/src/index.js',
+  output: {
+    path: './dist',
+    filename: 'application.bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {test: /.js$/, loader: 'eslint-loader', exclude: /node_modules/}
+    ],
+    loaders: [
+      {test: /.js$/, loader: 'babel-loader', exclude: /node_modules/},
+      {test: /.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader']}
+    ]
+  }
+};
